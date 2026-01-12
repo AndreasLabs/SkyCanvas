49626d2fb77480d8c41afd062d0306915d5a54f2
Alex Carter <victoryforphil@gmail.com>
2026-01-11 22:25:38 -0800

    Config update

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
 config = {
     rerun = true,
-    quad = {
-        connection_string = "tcpout://127.0.0.1:5760",
-        telemetry_rate_hz = 100
-    },
+}
+
+config.quad = {
+    connection_string = "tcpout://127.0.0.1:5760",
+    telemetry_rate_hz = 100
 }
